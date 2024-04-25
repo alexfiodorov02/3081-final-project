6ba982e1beffd15df26182b84407c69d48afe960
Alexander Fiodorov <alexfiodorov02@gmail.com>
2024-04-25 15:25:26 -0500

    Added root url redirect

--- djangoproject/djangoapp/urls.py
+++ djangoproject/djangoapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'djangoapp'
 
 urlpatterns = [
+    path('', views.employees, name='employees'),
     path('employees/', views.employees, name='employees'),
     path('customers/', views.customers, name='customers'),
     path('search/', views.search, name='search'),
