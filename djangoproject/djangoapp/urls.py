db8f98329a14305506e54c889506a568e0ea3b8d
Alexander Fiodorov <alexfiodorov02@gmail.com>
2024-04-25 16:01:25 -0500

    Implemented separate search functions for customers and employees

--- djangoproject/djangoapp/urls.py
+++ djangoproject/djangoapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.customers, name='customers'),
     path('employees/', views.employees, name='employees'),
     path('customers/', views.customers, name='customers'),
-    path('search/', views.search, name='search'),
+    path('search_customers/', views.search_customers, name='search_customers'),
+    path('search_employees/', views.search_employees, name='search_employees'),
 ]
--- djangoproject/djangoapp/views.py
+++ djangoproject/djangoapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Q
 from .models import Employee, Customer
+from django.urls import resolve
 
 def employees(request):
     employees = Employee.objects.all()
@@ -10,31 +11,18 @@ def customers(request):
     customers = Customer.objects.all()
     return render(request, 'customers.html', {'customers': customers})
 
-def search(request):
+def search_customers(request):
     query = request.GET.get('q')
     if query:
-        current_page = resolve(request.path_info).url_name
-
-        if current_page == 'customers':
-            results = Customer.objects.filter(
-                Q(first_name__icontains=query) |
-                Q(last_name__icontains=query) |
-                Q(phone_number__icontains=query) |
-                Q(email__icontains=query) |
-                Q(company__icontains=query)
-            )
-        elif current_page == 'employees':
-            results = Employee.objects.filter(
-                Q(first_name__icontains=query) |
-                Q(last_name__icontains=query) |
-                Q(phone_number__icontains=query) |
-                Q(email__icontains=query) |
-                Q(role__icontains=query)
-            )
+        results = Customer.objects.filter(Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(phone_number__icontains=query) | Q(email__icontains=query) | Q(company__icontains=query))
     else:
-        if current_page == 'customers':
-            results = Customer.objects.all()
-        elif current_page == 'employees':
-            results = Employee.objects.all()
+        results = Customer.objects.all()
+    return render(request, 'customers.html', {'customers': results})
 
-    return render(request, f'{current_page}.html', {current_page: results})
+def search_employees(request):
+    query = request.GET.get('q')
+    if query:
+        results = Employee.objects.filter(Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(phone_number__icontains=query) | Q(email__icontains=query) | Q(role__icontains=query))
+    else:
+        results = Employee.objects.all()
+    return render(request, 'employees.html', {'employees': results})
