b8e0ad1450dc94f51d2f600ec53eb74e861a3fc2
Alexander Fiodorov <alexfiodorov02@gmail.com>
2024-04-25 13:09:21 -0500

    Set up urls.py in project and app folders

---
+++ djangoproject/djangoapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'djangoapp'
+
+urlpatterns = [
+    path('employees/', views.employees, name='employees'),
+    path('customers/', views.customers, name='customers'),
+]
