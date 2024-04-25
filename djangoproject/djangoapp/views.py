698e0d505d33ae395ec5cf74e80eeabf62d50131
Alexander Fiodorov <alexfiodorov02@gmail.com>
2024-04-25 12:56:32 -0500

    Set up views.py

--- djangoproject/djangoapp/views.py
+++ djangoproject/djangoapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Employee, Customer
 
-# Create your views here.
+def employees(request):
+    employees = Employee.objects.all()
+    return render(request, 'employees.html', {'employees': employees})
+
+def customers(request):
+    customers = Customer.objects.all()
+    return render(request, 'customers.html', {'customers': customers})
