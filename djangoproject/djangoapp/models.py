1d87d98f08697526da4d0bc20788c807bc115cfe
Alexander Fiodorov <alexfiodorov02@gmail.com>
2024-04-25 11:47:32 -0500

    Set up models.py and ran makemigrations/migrate

--- djangoproject/djangoapp/models.py
+++ djangoproject/djangoapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator, MinLengthValidator, EmailValidator, RegexValidator
 
-# Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=255, validators=[MinLengthValidator(3), MaxLengthValidator(255)])
+    last_name = models.CharField(max_length=255, validators=[MinLengthValidator(3), MaxLengthValidator(255)])
+    phone_number = models.CharField(max_length=15, validators=[MinLengthValidator(10), MaxLengthValidator(15), RegexValidator(r'^\+?1?\d{9,15}$')])
+    email = models.EmailField(max_length=255, validators=[MinLengthValidator(3), MaxLengthValidator(255), EmailValidator()])
+    role = models.CharField(max_length=255, validators=[MinLengthValidator(3), MaxLengthValidator(255)])
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255, validators=[MinLengthValidator(3), MaxLengthValidator(255)])
+    last_name = models.CharField(max_length=255, validators=[MinLengthValidator(3), MaxLengthValidator(255)])
+    phone_number = models.CharField(max_length=15, validators=[MinLengthValidator(10), MaxLengthValidator(15), RegexValidator(r'^\+?1?\d{9,15}$')])
+    email = models.EmailField(max_length=255, validators=[MinLengthValidator(3), MaxLengthValidator(255), EmailValidator()])
+    company = models.CharField(max_length=255, validators=[MinLengthValidator(3), MaxLengthValidator(255)])
+
